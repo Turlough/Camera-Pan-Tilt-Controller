9aad9d7172e8237e0c84b980963ac78c9ef7e8a4
Turlough <turloughcowman@gmail.com>
2020-09-13 00:24:23 +0100

    Subscriber.py uses JsonLoader for config.

--- pi/subscriber.py
+++ pi/subscriber.py
@@ -6,19 +6,17 @@ These represent target degrees for each servo
 """
 import paho.mqtt.client as mqtt
 from servos import PanTilt
+from json_loader import JsonLoader
 
-# TODO Replace these with your own configuration
-host = '172.16.92.200'
-port = 1883
-topic = 'pan_tilt_controller'
-user = 'turlough'
-password = 'b00lab00la'
+config = JsonLoader()
+topic = ''
 
 # define pins. On the Pi zero, pins 18 and 12 support PWM on a hardware level. We use 18 and 12 here.
 controller = PanTilt(pan_pin=18, tilt_pin=12)
 
 
 def on_message(mosq, obj, msg):
+
     payload = msg.payload.decode("utf-8")
     # print(payload)
     values = payload.split(',')
@@ -28,15 +26,25 @@ def on_message(mosq, obj, msg):
 
 
 def on_connect(client, userdata, flags, rc):
+
     print("Connected with result code " + str(rc))
     client.subscribe(topic, 0)
 
 
-if __name__ == '__main__':
+def main():
+    global topic
+
     client = mqtt.Client()
     client.on_message = on_message
     client.on_connect = on_connect
 
-    client.username_pw_set(user, password)
-    client.connect(host, port, 60)
+    c = config.from_file('../mqtt_config.json')
+    topic = c.topic
+
+    client.username_pw_set(c.username, c.password)
+    client.connect(c.host, c.port, 60)
     client.loop_forever()
+
+
+if __name__ == '__main__':
+    main()
